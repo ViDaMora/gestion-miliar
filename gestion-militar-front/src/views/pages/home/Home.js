d4354893e6ea944e92831487d376aceedd1a9686
Brian Vanegas <brianvs18@gmail.com>
2021-07-27 15:24:30 -0500

    se crea la barra de navegacion de prueba

--- gestion-militar-front/src/views/pages/home/Home.js
+++ gestion-militar-front/src/views/pages/home/Home.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { GetSoldierInfo } from "../../../application/actions/SoldierAction";
+import Header from "../../components/Header";
 
 
 class Home extends Component {
+
+    componentDidMount() {
+        this.props.GetSoldierInfo()
+    }
+
     render() {
-        const { GetSoldierInfo } = this.props;
-        GetSoldierInfo({ name: "Brian" })
+
         return (
             <>
-                <h1>Home View</h1>
+                <Header />
+                <div className="container">
+
+                </div>
             </>
         )
     }
 }
 
-const mapDispatchToProps = {
-    GetSoldierInfo
+const mapDispatchToProps = (dispatch) => {
+    return {
+        GetSoldierInfo: () => dispatch(GetSoldierInfo()),
+    }
+
 }
 
 export default connect(null, mapDispatchToProps)(Home);

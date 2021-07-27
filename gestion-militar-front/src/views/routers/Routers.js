f67b330b511030a72a4fec48e2d8c6c6a4b99044
Brian Vanegas <brianvs18@gmail.com>
2021-07-26 23:47:03 -0500

    se crea estructura base para trabajar con redux

---
+++ gestion-militar-front/src/views/routers/Routers.js
@@ -0,0 +1,13 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Routers = () =>{
+    return(
+        <Router>
+            <Switch>
+                <Route />
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routers;

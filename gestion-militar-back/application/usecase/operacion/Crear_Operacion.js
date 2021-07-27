3da45be0d1d58f96604e51440ac799e3a70b60f3
Victor Mora <vdaniel.mora@udea.edu.co>
2021-07-27 09:08:59 -0500

    Implementacion de metodos para obtener entidades, y logica de negocio para algunos casos de uso del lider

---
+++ gestion-militar-back/application/usecase/operacion/Crear_Operacion.js
@@ -0,0 +1,33 @@
+const cadenaMando = require('../CadenaMando')
+const Operacion = require('../../../domain/Operacion/Operacion')
+
+
+// OP NO TRIVIAL
+async function Crear_Operacion(unidadId,lider,descripcion,pais,nombre,UnidadRepository,OperacionRepository,LiderRepository){
+
+
+    if(lider){
+        for(const unidadid in unidadId){
+            let unidadAsignada = await UnidadRepository.findById(unidadid)
+            if(cadenaMando.get(unidadAsignada.encargado.autoridad)<cadenaMando.get(lider.autoridad)){
+                return {
+                    errorMessage: "El lider no puede tener menor autoridad que un encargado de unidad"
+                }
+            }
+        }
+    }
+
+    let operacion = new Operacion(null,unidadId,lider,descripcion,pais,nombre)
+
+    let liderExiste = await LiderRepository.findById(lider.id)
+    if(!liderExiste){
+        return {
+            errorMessage: "El lider que intenta asignar no existe",
+            succes:false
+        }
+    }
+    await LiderRepository.updateActivo(lider.id)
+    return await OperacionRepository.save(operacion)
+}
+
+module.exports = {Crear_Operacion}
--- gestion-militar-back/application/usecase/vehiculo/Borrar_Vehiculo.js
+++ gestion-militar-back/application/usecase/vehiculo/Borrar_Vehiculo.js
@@ -2,7 +2,8 @@
 async function  borrarVehiculo(id,VehiculoRepository){
     let vehiculo= await VehiculoRepository.findById(id)
     if(!vehiculo){
-        return {errorMessage:"Vehiculo no Encontrado"}
+        return {errorMessage:"Vehiculo no Encontrado",
+        succes:false}
     }
     VehiculoRepository.delete(id)
     return {message: "Vehiculo eliminado con exito",

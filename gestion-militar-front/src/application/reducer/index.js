a101ee3f5268666bf943050d09c17fded78e5347
Brian Vanegas <brianvs18@gmail.com>
2021-07-28 11:21:21 -0500

    se crea la vista para lider, accion, reducer para redux, se trae lista con exito

--- gestion-militar-front/src/application/reducer/index.js
+++ gestion-militar-front/src/application/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SoldierInfo from './SoldierReducer';
+import lideresInfo from './LiderReducer';
 
 const reducer = combineReducers({
     soldiers: SoldierInfo,
+    lideres: lideresInfo,
 })
 
 export default reducer;
